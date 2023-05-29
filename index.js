d1c6632e09df455384afdea49dced8b0daf9cf0b
Dmitrijs Sinickis <dmitrijs.sinickis@gmail.com>
2023-05-29 13:17:08 +0300

    2905

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-const startBtn = document.getElementById("startBtn");
-const endElement = document.getElementById("demoversion");
+let startBtn = document.getElementById("startBtn");
+let endElement = document.getElementById("demoversion");
 
 startBtn.addEventListener("click", function () {
   endElement.scrollIntoView({ behavior: "smooth" });
 });
 
-const d3 = document.getElementById("d3");
+let d3 = document.getElementById("d3");
 d3.addEventListener("click", function () {
-  window.location.href = "d3.js_demo/d3.html";
+  window.location.href = "/demo/d3.js_demo/d3.html";
 });
 
-const cytoscape = document.getElementById("cytoscape");
+let cytoscape = document.getElementById("cytoscape");
 cytoscape.addEventListener("click", function () {
-  window.location.href = "cytoscape.js_demo/cytoscape.html";
+  window.location.href = "/demo/cytoscape.js_demo/cytoscape.html";
 });
 
-const vis = document.getElementById("vis");
+let vis = document.getElementById("vis");
 vis.addEventListener("click", function () {
-  window.location.href = "vis.js_demo/vis.html";
+  window.location.href = "/demo/vis.js_demo/vis.html";
+});
+
+let d3_random = document.getElementById("random_d3");
+d3_random.addEventListener("click", function () {
+  window.location.href = "random_generation/random_d3/random_d3.html";
+});
+
+let cytoscape_random = document.getElementById("random_cytoscape");
+cytoscape_random.addEventListener("click", function () {
+  window.location.href = "random_generation/random_cytoscape/random_cytoscape.html";
+});
+
+let vis_random = document.getElementById("random_vis");
+vis_random.addEventListener("click", function () {
+  window.location.href = "random_generation/random_vis/random_vis.html";
 });
