c03e9ffbeda710c44b4ea18c092167fe3380627a
Dmitrijs Sinickis <dmitrijs.sinickis@gmail.com>
2023-09-25 01:03:31 +0300

    add-btn&visPage

---
+++ scripts/buttons.js
@@ -0,0 +1,4 @@
+let d3Demo = () => window.location.href = "../demo/d3.js_demo/d3.html"
+let d3Experiment = () => window.location.href = "../random_generation/random_d3/random_d3.html"
+let cytoDemo = () => window.location.href = "../demo/cytoscape.js_demo/cytoscape.html"
+let cytoExperiment = () => window.location.href = "../random_generation/random_cytoscape/random_cytoscape.html"
--- scripts/chart.js
+++ scripts/chart.js
@@ -53,6 +53,35 @@ const cytoDataDoughunt = {
     },]
 }
 
+const visDataDoughunt = {
+    labels: [
+        'Loading',
+        'Scripting',
+        'Rendering',
+        'Painting',
+        'System',
+        'Idle',
+        'Total'
+    ],
+    datasets: [{
+        label: 'ms',
+        data: [3, 14595, 3, 0, 15, 40, 14657],
+        backgroundColor: [
+            'rgb(102, 36, 40)',
+            'rgb(245, 202, 23)',
+            'rgb(174, 30, 30)',
+            'rgb(69, 91, 145)',
+            'rgb(240, 81, 81)',
+            'rgb(118, 50, 107)',
+            'rgb(245, 121, 61)'
+
+        ]
+        ,
+        hoverOffset: 10,
+
+    },]
+}
+
 //CONFIG
 const d3ConfigDoughunt = {
     type: 'doughnut',
@@ -60,6 +89,13 @@ const d3ConfigDoughunt = {
     options: {
         maintainAspectRation: false,
         responsive: true,
+
+        plugins: {
+            title: {
+                display: true,
+                text: 'Results'
+            }
+        }
     }
 };
 
@@ -69,6 +105,27 @@ const cytoConfigDoughunt = {
     options: {
         maintainAspectRation: false,
         responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Results'
+            }
+        }
+    }
+};
+
+const visConfigDoughunt = {
+    type: 'doughnut',
+    data: visDataDoughunt,
+    options: {
+        maintainAspectRation: false,
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Results'
+            }
+        }
     }
 };
 
@@ -84,3 +141,10 @@ const cytoDouhunt = new Chart(
     cytoConfigDoughunt
 
 )
+
+const visDouhunt = new Chart(
+    document.getElementById('visChart_doughunt'),
+    visConfigDoughunt
+
+)
+
